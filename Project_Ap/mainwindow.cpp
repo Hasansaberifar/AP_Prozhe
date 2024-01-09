01c35ddd4bc2f2b218464ab4a04e601853edb2f1
Mobin <themobin2003@gmail.com>
2024-01-09 19:38:25 +0330

    create project

---
+++ Project_Ap/mainwindow.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    int i=0;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
